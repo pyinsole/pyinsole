df901ebc9b05ed29f762a72cb5e0f42fe6039a7c
Christian Hartung <hartung@live.com>
2024-12-10 12:14:59 -0300

    feat: add SNS message translator

--- tests/ext/test_routes.py
+++ tests/ext/test_routes.py
@@ -1,26 +1,45 @@
 from pyinsole.ext.aws.providers import SQSProvider
-from pyinsole.ext.aws.routes import SQSRoute
-from pyinsole.ext.aws.translators import SQSMessageTranslator
+from pyinsole.ext.aws.routes import SNSQueueRoute, SQSRoute
+from pyinsole.ext.aws.translators import SNSMessageTranslator, SQSMessageTranslator
 
 
-def test_sqs_route(dummy_handler):
-    route = SQSRoute("what", handler=dummy_handler)
-    assert isinstance(route.translator, SQSMessageTranslator)
-    assert isinstance(route.provider, SQSProvider)
-    assert route.name == "what"
+class TestSQSRoute:
+    def test_route(self, dummy_handler):
+        route = SQSRoute("what", handler=dummy_handler)
+        assert isinstance(route.translator, SQSMessageTranslator)
+        assert isinstance(route.provider, SQSProvider)
+        assert route.name == "what"
+
+    def test_keep_message_translator(self, dummy_handler, dummy_translator):
+        route = SQSRoute("what", handler=dummy_handler, translator=dummy_translator)
+        assert isinstance(route.translator, dummy_translator.__class__)
+
+    def test_keep_name(self, dummy_handler):
+        route = SQSRoute("what", handler=dummy_handler, name="foobar")
+        assert route.name == "foobar"
+
+    def test_provider_options(self, dummy_handler):
+        route = SQSRoute("what", handler=dummy_handler, provider_options={"use_ssl": False}, name="foobar")
+        assert "use_ssl" in route.provider._client_options  # noqa: SLF001
+        assert route.provider._client_options["use_ssl"] is False  # noqa: SLF001
 
 
-def test_sqs_route_keep_message_translator(dummy_handler, dummy_translator):
-    route = SQSRoute("what", handler=dummy_handler, translator=dummy_translator)
-    assert isinstance(route.translator, dummy_translator.__class__)
+class TestSNSQueueRoute:
+    def test_route(self, dummy_handler):
+        route = SNSQueueRoute("what", handler=dummy_handler)
+        assert isinstance(route.translator, SNSMessageTranslator)
+        assert isinstance(route.provider, SQSProvider)
+        assert route.name == "what"
 
+    def test_keep_message_translator(self, dummy_handler, dummy_translator):
+        route = SNSQueueRoute("what", handler=dummy_handler, translator=dummy_translator)
+        assert isinstance(route.translator, dummy_translator.__class__)
 
-def test_sqs_route_keep_name(dummy_handler):
-    route = SQSRoute("what", handler=dummy_handler, name="foobar")
-    assert route.name == "foobar"
+    def test_keep_name(self, dummy_handler):
+        route = SNSQueueRoute("what", handler=dummy_handler, name="foobar")
+        assert route.name == "foobar"
 
-
-def test_sqs_route_provider_options(dummy_handler):
-    route = SQSRoute("what", handler=dummy_handler, provider_options={"use_ssl": False}, name="foobar")
-    assert "use_ssl" in route.provider._client_options  # noqa: SLF001
-    assert route.provider._client_options["use_ssl"] is False  # noqa: SLF001
+    def test_provider_options(self, dummy_handler):
+        route = SNSQueueRoute("what", handler=dummy_handler, provider_options={"use_ssl": False}, name="foobar")
+        assert "use_ssl" in route.provider._client_options  # noqa: SLF001
+        assert route.provider._client_options["use_ssl"] is False  # noqa: SLF001
--- tests/ext/test_translator.py
+++ tests/ext/test_translator.py
@@ -2,45 +2,97 @@ import json
 
 import pytest
 
-from pyinsole.ext.aws.translators import SQSMessageTranslator
+from pyinsole.ext.aws.translators import SNSMessageTranslator, SQSMessageTranslator
 
 
-@pytest.fixture
-def sqs_translator():
-    return SQSMessageTranslator()
+class TestSQSMessageTranslator:
+    def test_translate_sqs(self):
+        original = {"Body": json.dumps("some-content")}
+        sqs_translator = SQSMessageTranslator()
+
+        content = sqs_translator.translate(original)
+
+        assert "content" in content
+        assert content["content"] == "some-content"
+
+        original = {"Body": json.dumps({"key": "value"})}
+        content = sqs_translator.translate(original)
+        assert content["content"] == {"key": "value"}
+
+    def test_sqs_metadata_extract(self):
+        original = {"Body": json.dumps("some-content"), "whatever": "whatever"}
+        sqs_translator = SQSMessageTranslator()
+
+        content = sqs_translator.translate(original)
+
+        metadata = content["metadata"]
+        assert metadata
+        assert "whatever" in metadata
+        assert metadata["whatever"] == "whatever"
+
+    @pytest.mark.parametrize("message", [{"invalid": "format"}, "invalid format", 42, {}, [], (), ""])
+    def test_translate_sqs_handles_invalid_format(self, message):
+        sqs_translator = SQSMessageTranslator()
+
+        content = sqs_translator.translate(message)
+
+        assert content["content"] is None
+
+    def test_translate_sqs_handles_json_error(self):
+        original = {"Body": "invalid: json"}
+        sqs_translator = SQSMessageTranslator()
+
+        content = sqs_translator.translate(original)
+
+        assert content["content"] is None
 
 
-def test_translate_sqs(sqs_translator):
-    original = {"Body": json.dumps("some-content")}
-    content = sqs_translator.translate(original)
-    assert "content" in content
-    assert content["content"] == "some-content"
+class TestSNSMessageTranslator:
+    def test_translate_sns(self):
+        message_content = "here I am"
+        message = json.dumps({"Message": json.dumps(message_content)})
+        original = {"Body": message}
+        sns_translator = SNSMessageTranslator()
 
-    original = {"Body": json.dumps({"key": "value"})}
-    content = sqs_translator.translate(original)
-    assert content["content"] == {"key": "value"}
+        content = sns_translator.translate(original)
 
+        assert content["content"] == message_content
 
-def test_sqs_metadata_extract(sqs_translator):
-    original = {"Body": json.dumps("some-content"), "whatever": "whatever"}
-    content = sqs_translator.translate(original)
-    metadata = content["metadata"]
-    assert metadata
-    assert "whatever" in metadata
-    assert metadata["whatever"] == "whatever"
+        message_content = {"here": "I am"}
+        message = json.dumps({"Message": json.dumps(message_content)})
+        original = {"Body": message}
+        content = sns_translator.translate(original)
+        assert content["content"] == message_content
 
+    def test_sns_metadata_extract(self):
+        message_content = "here I am"
+        message = json.dumps({"Message": json.dumps(message_content), "foo": "nested"})
+        original = {"Body": message, "bar": "not nested"}
+        sns_translator = SNSMessageTranslator()
 
-@pytest.fixture(params=[{"invalid": "format"}, "invalid format", 42, {}, [], (), ""])
-def parametrize_invalid_messages(request):
-    return request.param
+        content = sns_translator.translate(original)
 
+        metadata = content["metadata"]
+        assert metadata
+        assert "foo" in metadata
+        assert metadata["foo"] == "nested"
+        assert "bar" in metadata
+        assert metadata["bar"] == "not nested"
 
-def test_translate_sqs_handles_invalid_format(sqs_translator, parametrize_invalid_messages):
-    content = sqs_translator.translate(parametrize_invalid_messages)
-    assert content["content"] is None
+    @pytest.mark.parametrize("content", [{"invalid": "format"}, "invalid format", 42, {}, [], (), ""])
+    def test_translate_sns_handles_invalid_content(self, content):
+        message = json.dumps({"Message": content})
+        original = {"Body": message}
+        sns_translator = SNSMessageTranslator()
 
+        content = sns_translator.translate(original)
 
-def test_translate_sqs_handles_json_error(sqs_translator):
-    original = {"Body": "invalid: json"}
-    content = sqs_translator.translate(original)
-    assert content["content"] is None
+        assert content["content"] is None
+
+    @pytest.mark.parametrize("message", [{"invalid": "format"}, "invalid format", 42, {}, [], (), ""])
+    def test_translate_sns_handles_invalid_format(self, message):
+        sns_translator = SNSMessageTranslator()
+
+        content = sns_translator.translate(message)
+
+        assert content["content"] is None
