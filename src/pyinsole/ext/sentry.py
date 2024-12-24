d79b964cddb988369b1f460f69eb7b83a073055d
Christian Hartung <hartung@live.com>
2024-12-24 11:57:32 -0300

    perf: make sentry error handler async to avoid context switching

--- src/pyinsole/ext/sentry.py
+++ src/pyinsole/ext/sentry.py
@@ -2,7 +2,7 @@ from sentry_sdk import Hub
 
 
 def sentry_handler(sdk_or_hub: Hub, *, delete_message: bool = False):
-    def send_to_sentry(exc_info, message):
+    async def send_to_sentry(exc_info, message):
         with sdk_or_hub.push_scope() as scope:
             scope.set_extra("message", message)
             sdk_or_hub.capture_exception(exc_info)
