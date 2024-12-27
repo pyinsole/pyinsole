4349728a5cc5b490dfcbad2f930d97743947b1ed
Christian Hartung <hartung@live.com>
2024-12-27 17:14:47 -0300

    fix: AbstractDispatcher should be async

--- src/pyinsole/_compat.py
+++ src/pyinsole/_compat.py
@@ -4,10 +4,14 @@ PY312 = sys.version_info >= (3, 12)
 
 if PY312:
     from inspect import iscoroutinefunction
+    from typing import override
 else:
     from asyncio import iscoroutinefunction
+
+    from typing_extensions import override
 
 
 __all__ = [
     "iscoroutinefunction",
+    "override",
 ]
